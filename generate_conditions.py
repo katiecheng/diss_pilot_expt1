dcf331f684a7e9e153a7455ee8d856aa8a6b0718
Katie Cheng <katie.cheng@gmail.com>
2019-10-18 19:24:24 -0700

    remove .DS_Store, add create_conditions.html

--- generate_conditions.py
+++
@@ -1,9 +0,0 @@
-# Import database module.
-import firebase_admin
-from firebase_admin import db
-
-# Get a database reference to our posts
-ref = db.reference('server/saving-data/fireblog/posts')
-
-# Read the data at the posts reference (this is a blocking operation)
-print(ref.get())
